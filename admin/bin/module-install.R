344cf1c373557023dd242698b8d2063bf290b42a
wharrell <wharrell@mit.edu>
2016-07-14 12:02:37 -0400

    added some admin R scripts, a new R library module config

---
+++ admin/bin/module-install.R
@@ -0,0 +1,37 @@
+## Central Install R modules script
+# Install all new and updated R modules from a number of repos to NFS
+
+
+# Global option to not test before loading.  We might get a few broken pkgs but
+# we also get some packages
+# that build OK but for some reason fail a test.  We can look into these if
+# users complain.
+
+options( INSTALL_opts = "--no-test-load" )
+
+
+##
+## CRAN repo
+#
+options( repos = "ftp://mirror.hmdc.harvard.edu/CRAN" )
+# Get new modules
+plist <- new.packages(lib.loc = "/home/software/sloan/local/lib/R/el6")
+sapply(plist,function(x){try(install.packages(x,lib="/home/software/sloan/local/lib/R/el6"))})
+# Get updated modules
+ulist <- old.packages(lib.loc = "/home/software/sloan/local/lib/R/el6")
+sapply(ulist,function(x){try(install.packages(x,lib="/home/software/sloan/local/lib/R/el6"))})
+
+##
+## Omegahat repo
+#
+options( repos = "http://www.omegahat.org/R/" )
+# Get new modules
+plist <- new.packages(lib.loc = "/home/software/sloan/local/lib/R/el6")
+sapply(plist,function(x){try(install.packages(x,lib="/home/software/sloan/local/lib/R/el6"))})
+# Get updated modules
+ulist <- old.packages(lib.loc = "/home/software/sloan/local/lib/R/el6")
+sapply(ulist,function(x){try(install.packages(x,lib="/home/software/sloan/local/lib/R/el6"))})
+
+# Save any warnings
+cat (file="warnings.rinstall")
+warnings(file="warnings.rinstall",append=TRUE)
