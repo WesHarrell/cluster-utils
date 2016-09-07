d4e33556342ed7bcf1a6f939368a6cb1a5f14b49
Wesley Harrell <wharrell@mit.edu>
2016-09-07 15:34:34 -0400

    added a few more py mods

--- admin/bin/pymod-install.sh
+++ admin/bin/pymod-install.sh
@@ -8,3 +8,14 @@ pip2.7 install pandas --target=/home/software/sloan/local/lib/py27 --upgrade
 pip2.7 install matplotlib --target=/home/software/sloan/local/lib/py27 --upgrade
 pip2.7 install scipy --target=/home/software/sloan/local/lib/py27 --upgrade
 pip2.7 install mpmath --target=/home/software/sloan/local/lib/py27 --upgrade
+pip2.7 install csvkit --target=/home/software/sloan/local/lib/py27 --upgrade
+pip2.7 install prettyplotlib --target=/home/software/sloan/local/lib/py27 --upgrade
+pip2.7 install plotly --target=/home/software/sloan/local/lib/py27 --upgrade
+pip2.7 install ggplot --target=/home/software/sloan/local/lib/py27 --upgrade
+pip2.7 install Bokeh --target=/home/software/sloan/local/lib/py27 --upgrade
+pip2.7 install PyMC --target=/home/software/sloan/local/lib/py27 --upgrade
+pip2.7 install Scikit-learn --target=/home/software/sloan/local/lib/py27 --upgrade
+pip2.7 install SymPy --target=/home/software/sloan/local/lib/py27 --upgrade
+pip2.7 install Statsmodels --target=/home/software/sloan/local/lib/py27 --upgrade
+pip2.7 install jupyter-core --target=/home/software/sloan/local/lib/py27 --upgrade
+
